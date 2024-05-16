86a555641aacaab950ccb8103f5da6e2480e7756
Tobias Bay - Windows <tobiasabay@gmail.com>
2024-05-16 18:48:01 +0200

    modified:   src/pages/PasswordPage.css 	modified:   src/pages/RecipesPage.js

--- src/pages/RecipesPage.js
+++ src/pages/RecipesPage.js
@@ -9,6 +9,15 @@ export default function RecipesPage() {
             <h1 className="recipespage-header">
                 <span className="recipes-text">Recipes</span> Page
             </h1>
+
+            <div className="recipe-form">
+                <input type="text" placeholder="Title" />
+                <input type="text" placeholder="Amount" />
+                <input type="text" placeholder="Ingredients" />
+                <input type="text" placeholder="Description" />
+                <input type="text" placeholder="Notes" />
+                <button>Add Row</button>
+            </div>
         </div>
     )
 }
