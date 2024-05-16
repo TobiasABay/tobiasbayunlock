817a2459223f73952cfc1e1126784dd80161d493
Tobias Bay - Windows <tobiasabay@gmail.com>
2024-05-16 21:35:35 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/RecipesPage.js

--- src/pages/RecipesPage.js
+++ src/pages/RecipesPage.js
@@ -1,24 +1,47 @@
+import { Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
 import Navbar from './Navbar';
-import './RecipesPage.css';
 
 export default function RecipesPage() {
-
     return (
-        <div className="recipes-container">
-            <Navbar></Navbar>
-            <h1 className="recipespage-header">
-                <span className="recipes-text">Recipes</span> Page
-            </h1>
+        <Box sx={{ 
+            backgroundColor: '#222', 
+            minHeight: '100vh', 
+            padding: 10, 
+            display: 'flex', 
+            flexDirection: 'column', 
+            alignItems: 'center' 
+            }}>
 
-            <div className="recipe-form">
-                <input type="text" placeholder="Title" />
-                <input type="text" placeholder="Amount" />
-                <input type="text" placeholder="Ingredients" />
-                <input type="text" placeholder="Description" />
-                <input type="text" placeholder="Notes" />
-                <button>Add Row</button>
-            </div>
-        </div>
-    )
+            <Navbar />
+            <Container maxWidth="md">
+                <Box sx={{display: 'flex', justifyContent: 'center', textAlign: 'center', backgroundColor: '#222'}}>
+                <Typography
+                    variant="h3"
+                    component="h1"
+                    gutterBottom
+                    sx={{ color: '#007bff', marginTop: 4 }}
+                >
+                    Recipes
+                </Typography>
+                <Box sx={{width: 16}}></Box>
+                <Typography
+                    variant="h3"
+                    component="h1"
+                    gutterBottom
+                    sx={{ color: '#ffffff', marginTop: 4 }}
+                    >
+                 Page
+                </Typography>
+                </Box>
+                <Paper elevation={3} sx={{ display: 'flex', flexDirection: 'column',alignItems: 'center', padding: 3, borderRadius: 2}}>
+                    <TextField id="title" label="Title" variant="outlined" fullWidth margin="normal" />
+                    <TextField id="amount" label="Amount" variant="outlined" fullWidth margin="normal" />
+                    <TextField id="ingredients" label="Ingredients" variant="outlined" fullWidth margin="normal" />
+                    <TextField id="description" label="Description" variant="outlined" fullWidth margin="normal" />
+                    <TextField id="notes" label="Notes" variant="outlined" fullWidth margin="normal" />
+                    <Button variant="contained" color="primary" sx={{maxWidth: 6, margin: 1}}>Submit</Button>
+                </Paper>
+            </Container>
+        </Box>
+    );
 }
-
